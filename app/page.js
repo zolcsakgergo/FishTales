72fa4e36cc6036230b0e6c7130a57de56e4b5687
Zolcsák Gergő <zolcsakgergo2001@gmail.com>
2024-03-14 00:31:16 +0100

    amplify hotfix

--- app/page.js
+++ app/page.js
@@ -8,12 +8,6 @@ export default function Home() {
       <Head>
         <title>FishTales</title>
         <link rel="icon" href="/public/favicon.ico" />
-        <script src="https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.js"></script>
-        <link
-          type="text/css"
-          rel="stylesheet"
-          href="https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.css"
-        />
       </Head>
       <main>
         <h1>Fish Tales</h1>
